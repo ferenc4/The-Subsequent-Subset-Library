cb0a42b8593ccf8ef71530779905e332afa4962f
Ferenc <ferenc4.fazekas@gmail.com>
2019-06-28 00:08:11 +1000

    added wip test

--- src/test/java/subset/SubsetStreamTest.java
+++ src/test/java/subset/SubsetStreamTest.java
@@ -2,8 +2,12 @@ package subset;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * @author <a href="https://github.com/ferenc4">Ferenc Fazekas</a>
@@ -16,6 +20,15 @@ public class SubsetStreamTest {
                 .filter(it -> !it.startsWith("p"))
                 .map(String::toUpperCase)
                 .collect();
+        System.out.println("actual = " + actual);
+    }
+
+    @Test
+    public void testData1() {
+        InputStream resourceAsStream = SubsetStreamTest.class.getClassLoader().getResourceAsStream("data1.txt");
+        Stream<Integer> integerStream = new BufferedReader(new InputStreamReader(resourceAsStream)).lines()
+                .map(Integer::valueOf);
+        SubSetStreamResult<Integer> actual = new SubsetStream<>(integerStream, 500).collect();
         System.out.println("actual = " + actual);
     }
 }
